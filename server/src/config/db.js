171819901e24748de08babced62c956874fa6671
ariefzealzoft <arief@zealzoft.com>
2026-07-20 12:16:17 +0530

    Fix PostgreSQL SSL connection for Render

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,10 +6,9 @@ dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
