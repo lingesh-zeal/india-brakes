57c0bab2e8724719edcbecaf8cbea9808dea51f9
ariefzealzoft <arief@zealzoft.com>
2026-07-20 10:26:55 +0530

    Prepare backend for Render deployment

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,4 +7,7 @@ const {Pool} = pg;
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl:
+    process.env.NODE_ENV === "production"
+    ? {rejectUnauthorized: false} : false,
 });
